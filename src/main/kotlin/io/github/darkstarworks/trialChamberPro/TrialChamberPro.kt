84b76d6df74c08900e5ca6e8373088c062cb59ce
darkstarworks <darkstarworks@gmail.com>
2025-10-24 21:06:12 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/darkstarworks/trialChamberPro/TrialChamberPro.kt
@@ -0,0 +1,14 @@
+package io.github.darkstarworks.trialChamberPro
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TrialChamberPro : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
